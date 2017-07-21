567a87b97a2fbe8dd04179f35db00401151f1413
Hyunuk Jung <hujung@baikal.io>
2017-07-21 10:01:35 +0800

    first commit

---
+++ public/config.js
@@ -0,0 +1,9 @@
+var config = {
+	apiKey: "AIzaSyCQljdlC2-c_S6MoZlkn5qerIyHU9a54S8",
+	authDomain: "socialdev-81b61.firebaseapp.com",
+	databaseURL: "https://socialdev-81b61.firebaseio.com",
+	projectId: "socialdev-81b61",
+	storageBucket: "socialdev-81b61.appspot.com",
+	messagingSenderId: "262170293950"
+};
+firebase.initializeApp(config);
